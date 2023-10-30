113d184069ad11431559a333713dce0102104b59
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-10-30 18:10:17 -0300

    feat: implemented registerPayment layout

---
+++ app/src/main/java/com/example/splitthebill/RegisterPaymentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.splitthebill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterPaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_payment)
+    }
+}
