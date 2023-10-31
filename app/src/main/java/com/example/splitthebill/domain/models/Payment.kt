bec82f6ffce10af5160f2eb1fc7741e60ea9d8bf
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-10-30 23:43:51 -0300

    fix: removed uncorrect get and setters

--- app/src/main/java/com/example/splitthebill/domain/models/Payment.kt
+++ app/src/main/java/com/example/splitthebill/domain/models/Payment.kt
@@ -4,29 +4,4 @@ class Payment(
     var name: String,
     var amountPaid: Double,
     var whatWasBought: String
-) {
-    // Getters and Setters
-    fun getName(): String {
-        return name
-    }
-
-    fun setName(name: String) {
-        this.name = name
-    }
-
-    fun getAmountPaid(): Double {
-        return amountPaid
-    }
-
-    fun setAmountPaid(amountPaid: Double) {
-        this.amountPaid = amountPaid
-    }
-
-    fun getWhatWasBought(): String {
-        return whatWasBought
-    }
-
-    fun setWhatWasBought(whatWasBought: String) {
-        this.whatWasBought = whatWasBought
-    }
-}
+)
