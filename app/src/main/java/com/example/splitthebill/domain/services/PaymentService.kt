26a20ca9ece14eeb0dbfaf6a23304a74ea627b76
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-10-30 23:44:27 -0300

    feat: implemented Payment service

---
+++ app/src/main/java/com/example/splitthebill/domain/services/PaymentService.kt
@@ -0,0 +1,22 @@
+package com.example.splitthebill.domain.services
+
+import com.example.splitthebill.domain.models.Payment
+
+class PaymentService {
+    private val paymentList = mutableListOf<Payment>()
+
+    // Adicionar um pagamento à lista
+    fun addPayment(payment: Payment) {
+        paymentList.add(payment)
+    }
+
+    // Remover um pagamento pelo nome
+    fun removePaymentByName(name: String) {
+        paymentList.removeAll { it.name == name }
+    }
+
+    // Retornar a lista de pagamentos
+    fun getAllPayments(): List<Payment> {
+        return paymentList
+    }
+}
